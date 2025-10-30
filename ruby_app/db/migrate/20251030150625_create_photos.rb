ecd5e7bd1aeffe337d6ffc6c55780a2793615fd3
gustavomgama <gutomgama@gmail.com>
2025-10-30 12:07:34 -0300

    Generates Property and Photo models

---
+++ ruby_app/db/migrate/20251030150625_create_photos.rb
@@ -0,0 +1,13 @@
+class CreatePhotos < ActiveRecord::Migration[8.0]
+  def change
+    create_table :photos do |t|
+      t.references :property, null: false, foreign_key: true
+      t.string :filename
+      t.integer :position
+      t.string :content_type
+      t.integer :file_size
+
+      t.timestamps
+    end
+  end
+end
